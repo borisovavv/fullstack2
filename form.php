d73d71069f72d1045c48e5af6701b5e5f6d622b4
borisovavv <borisova.victoria@mail.ru>
2021-12-22 02:30:27 +0300

    форма создания пользователя

--- form.php
+++ form.php
@@ -1,6 +1,22 @@
 <?php
 
-$name = $_POST['name'];
-$age = $_POST['age'];
+$user = 'root';
+$pdo = new Pdo('mysql:dbname=fullstack2;host=127.0.0.1', $user);
 
-echo "Hello, $name, you are $age years old!";
+$name = $_POST['name'];
+$login = $_POST['login'];
+$password = $_POST['password'];
+$city_id = $_POST['city_id'];
+
+//Запрос с "заглушками" для безопасной передачи данных в базу
+$query = "INSERT INTO users (name, login, password, city_id) VALUES(:name, :login, :password, :city_id)";
+$res = $pdo->prepare($query);
+//Выполнение запроса
+$res->execute([
+    ':name' => $name,
+    ':login' => $login,
+    ':password' => $password,    
+    ':city_id' => $city_id,
+]);
+
+header('Location: index.php');
